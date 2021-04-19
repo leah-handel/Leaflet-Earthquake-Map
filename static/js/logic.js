f0eb2c340762431a871d8f50fa4aa9373abe300e
leah-handel <leah.handel@gmail.com>
2021-04-18 20:27:22 -0500

    circles have variable radius and color

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 var map = L.map("map", {
-    center: [45, -115],
-    zoom: 4
+    center: [40, -115],
+    zoom: 5
   });
   
   // Adding a tile layer (the background map image) to our map:
@@ -22,11 +22,28 @@ d3.json(earthquakeURL).then(function(data) {
 
         var location = [coordinates[1], coordinates[0]];
 
+        var depth = coordinates [2];
+
+        var color = "#648FFF";
+
+        if (depth < 5) {
+            color = "#FFB000";
+        } else if (depth < 10) {
+            color = "#785EF0";
+        } else if (depth < 20) {
+            color = "#DC267F";
+        } else if (depth <50) {
+            color = "#FE6100";
+        }
+
+        var magnitude = quake.properties.mag;
+
         var marker = L.circle(location, {
-            fillOpacity: 0.75,
-            color: "red",
-            fillColor: "purple",
-            radius: 1000
+            fillOpacity: 0.9,
+            color: "black",
+            weight: 1,
+            fillColor: color,
+            radius: 10000*magnitude
           })
           .bindPopup(quake.properties.place);
 
